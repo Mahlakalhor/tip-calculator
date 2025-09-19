a6c693845bc484916ccf74786f07ac4c7df32782
Mahla <Mahlakalhor82@gmail.com>
2025-09-19 16:49:31 -0700

    v1

---
+++ script.js
@@ -0,0 +1,5 @@
+const billEl = document.getElementById("bill");
+const tipEl = document.getElementById("tip");
+const calculateEl = document.getElementById("calculate");
+const totalEl = document.getElementById("total");
+
