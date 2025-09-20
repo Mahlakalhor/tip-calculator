a4f9a5070c345cf9bb359474de95d5343b1959c7
Mahla <Mahlakalhor82@gmail.com>
2025-09-19 17:03:12 -0700

    v

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const tipEl = document.getElementById("tip");
 const calculateEl = document.getElementById("calculate");
 const totalEl = document.getElementById("total");
 
+calculateEl.addEventListener("click",()=>{
+    const sum = Number(billEl.value) + Number(tipEl.value);
+    totalEl.innerText= sum ;
+})
